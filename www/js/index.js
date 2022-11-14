5c041cc7808d9d0e2bb9a4e5a3a7143e6041d982
sarkadie <sarkadie@uleth.ca>
2022-11-14 11:44:30 -0700

    set up camera app

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,33 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    // use plugins here
+
+    var options = {
+        quality: 80,
+        destinationType: Camera.DestinationType.FILE_URI
+
+    }
+
+    $("#takePhoto").on("click", takePic);
+    
+    function takePic() {
+        navigator.camera.getPicture(onSuccess, onError, options);
+    }
+
+    function onSuccess(imageData) { //imageData is the photo taken
+        console.log(imageData);
+        resolveLocalFileSystemURL(imageData, function (fileEntry) {
+            var myNewImage = fileEntry.toURL()
+            console.log(myNewImage);
+            // do something with URL, assign to src or create an html 
+            $("#takePhoto").after("<img src='" + myNewImage + "'>") //concatenation html || everything inside the double quotes is what's added
+        }, onError);
+
+    }
+    function onError(message) {
+        alert("Photo not taken because " + message)
+    }
 
 }
+
