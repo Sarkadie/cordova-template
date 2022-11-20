69dfa284e376478215513088ad1f74b02e69912b
sarkadie <sarkadie@uleth.ca>
2022-11-20 12:47:03 -0700

    changed colors and added designs

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
 
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -34,49 +35,3 @@ function onDeviceReady() {
 
 
 
-
-
-
-
-
-
-// //https://www.w3schools.com/howto/howto_js_image_grid.asp
-// var elements = document.getElementsByClassName("column");
-
-// // Declare a loop variable
-// var i;
-
-// // Full-width images
-// function one() {
-//     for (i = 0; i < elements.length; i++) {
-//     elements[i].style.msFlex = "100%";  // IE10
-//     elements[i].style.flex = "100%";
-//   }
-// }
-
-// // Two images side by side
-// function two() {
-//   for (i = 0; i < elements.length; i++) {
-//     elements[i].style.msFlex = "50%";  // IE10
-//     elements[i].style.flex = "50%";
-//   }
-// }
-
-// // Four images side by side
-// function four() {
-//   for (i = 0; i < elements.length; i++) {
-//     elements[i].style.msFlex = "25%";  // IE10
-//     elements[i].style.flex = "25%";
-//   }
-// }
-
-// // Add active class to the current button (highlight it)
-// var header = document.getElementById("myHeader");
-// var btns = header.getElementsByClassName("btn");
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function() {
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
